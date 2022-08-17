066d77b9b4e04b18f83ee8b4459ddff33c777e10
Rowell <dev.rowellpica@gmail.com>
2022-08-17 09:55:11 +0800

    updated readme and default db connection

--- config/datastores.js
+++ config/datastores.js
@@ -48,8 +48,8 @@ module.exports.datastores = {
     *    (See https://sailsjs.com/config/datastores for help.)                 *
     *                                                                          *
     ***************************************************************************/
-    // adapter: 'sails-mysql',
-    // url: 'mysql://user:password@host:port/database',
+    adapter: 'sails-mysql',
+    url: 'mysql://hkcadbuser:xvw56kLnEev5tz6g@localhost:3306/hkcadb',
 
   },
 
