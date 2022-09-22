30a8b21389eef029be77af28f367cbbca59b499d
Simon Sun <blackghost0118@gmail.com>
2022-09-22 09:32:34 +0800

    feat: add easy chart from database

--- api/controllers/dashboard/view-welcome.js
+++ api/controllers/dashboard/view-welcome.js
@@ -1,3 +1,5 @@
+const Promise = require('bluebird');
+
 module.exports = {
 
 
@@ -19,9 +21,52 @@ module.exports = {
 
   fn: async function () {
 
-    return {};
+    const company = this.req.me.company;
+
+    const indicators = await ESGIndicator.find({
+      company: company
+    });
+
+    const years = _.map(indicators, (indicator) => {
+      return indicator.yearOfRecord
+    });
+
+    const usageOfCombustibleFuelValues = [];
+    const usageOfCombustibleFuelPetrolValues = [];
+    const usageOfCombustibleFuelDieselValues = [];
+
+    await Promise.each(indicators, async (indicator) => {
+
+      const [
+        usageOfCombustibleFuelData,
+        usageOfCombustibleFuelPetrolData,
+        usageOfCombustibleFuelDieselData
+      ] = await Promise.all([
+        ESGIndicatorValue.findOne({
+          indicator: indicator.id,
+          key: 'usageOfCombustibleFuel'
+        }),
+        ESGIndicatorValue.findOne({
+          indicator: indicator.id,
+          key: 'usageOfCombustibleFuelPetrol'
+        }),
+        ESGIndicatorValue.findOne({
+          indicator: indicator.id,
+          key: 'usageOfCombustibleFuelDiesel'
+        }),
+      ]);
+
+      usageOfCombustibleFuelValues.push(_.round(usageOfCombustibleFuelData.value, 2))
+      usageOfCombustibleFuelPetrolValues.push(_.round(usageOfCombustibleFuelPetrolData.value, 2))
+      usageOfCombustibleFuelDieselValues.push(_.round(usageOfCombustibleFuelDieselData.value, 2))
+    });
+
+    return {
+      labels: years,
+      usageOfCombustibleFuelValues,
+      usageOfCombustibleFuelPetrolValues,
+      usageOfCombustibleFuelDieselValues
+    };
 
   }
-
-
 };
