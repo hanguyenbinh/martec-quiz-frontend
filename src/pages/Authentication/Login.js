da3bc60d4956bae82cf5e3a0265e830c8504daba
viethungdaniel <37524527+viethungdaniel@users.noreply.github.com>
2022-10-10 17:19:43 +0700

    Update Login.js

--- src/pages/Authentication/Login.js
+++ src/pages/Authentication/Login.js
@@ -1,193 +1,334 @@
-import React, { useEffect, useState } from 'react';
-import { Card, CardBody, Col, Container, Input, Label, Row, Button, Form, FormFeedback, Alert } from 'reactstrap';
-import ParticlesAuth from "../AuthenticationInner/ParticlesAuth";
+import React, { useEffect, useState } from "react"
+import {
+	Card,
+	CardBody,
+	Col,
+	Container,
+	Input,
+	Label,
+	Row,
+	Button,
+	Form,
+	FormFeedback,
+	Alert
+} from "reactstrap"
+import ParticlesAuth from "../AuthenticationInner/ParticlesAuth"
 
 //redux
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector, useDispatch } from "react-redux"
 
-import { withRouter } from "react-router-dom";
+import { withRouter } from "react-router-dom"
 
 // Formik validation
-import * as Yup from "yup";
-import { useFormik } from "formik";
+import * as Yup from "yup"
+import { Formik, useFormik } from "formik"
 
 // actions
-import { loginInitiate, loginUser, resetLoginFlag } from "../../store/actions";
-import { withTranslation } from 'react-i18next';
+import { loginInitiate, loginUser, resetLoginFlag } from "../../store/actions"
+import { withTranslation } from "react-i18next"
+
+const EMAIL_INPUT_FORM_TAB_INDEX = 0
+const OTP_CODE_INPUT_FORM_TAB_INDEX = 1
 
 const Login = (props) => {
-    const T = props.t;
-    const dispatch = useDispatch();
-    const { user } = useSelector(state => ({
-        user: state.Account.user,
-    }));
+	const [tabIndex, setTabIndex] = React.useState(EMAIL_INPUT_FORM_TAB_INDEX)
 
-    const [userLogin, setUserLogin] = useState([]);
-    const [userOtp, setUserOtp] = useState([])
+	const T = props.t
+	const dispatch = useDispatch()
+	const { user } = useSelector((state) => ({
+		user: state.Account.user
+	}))
 
-    useEffect(() => {
-        if (user && user) {
-            setUserLogin({
-                email: user.user.email,
-                // password: user.user.confirm_password
-            });
-        }
-    }, [user]);
+	const [userLogin, setUserLogin] = useState([])
+	const [userOtp, setUserOtp] = useState([])
 
-    const validation = useFormik({
-        // enableReinitialize : use this flag when initial values needs to be changed
-        enableReinitialize: true,
+	useEffect(() => {
+		if (user && user) {
+			setUserLogin({
+				email: user.user.email
+				// password: user.user.confirm_password
+			})
+		}
+	}, [user])
 
-        initialValues: {
-            email: userLogin.email || "hanguyenbinh201282@gmail.com" || '',
-            // password: userLogin.password || "123456" || '',
-        },
-        validationSchema: Yup.object({
-            email: Yup.string().required("Please Enter Your Email"),
-            // password: Yup.string().required("Please Enter Your Password"),
-        }),
-        onSubmit: (values) => {
-            dispatch(loginInitiate(values.email, props.history));
-        }
-    });
-    const otpValidation = useFormik({
-        // enableReinitialize : use this flag when initial values needs to be changed
-        enableReinitialize: true,
+	const { error } = useSelector((state) => ({
+		error: state.Login.error
+	}))
 
-        initialValues: {
-            otp: '',
-        },
-        validationSchema: Yup.object({
-            otp: Yup.string().length(6).required("Please Enter OTP"),
-        }),
-        onSubmit: (values) => {
-            dispatch(loginInitiate(values.email, props.history));
-        }
-    });
+	const { challengeId } = useSelector((state) => ({
+		challengeId: state.Login.challengeId
+	}))
 
-    const { error } = useSelector(state => ({
-        error: state.Login.error,
-    }));
+	const formikRef = React.useRef()
 
-    const { challengeId } = useSelector(state => ({
-        challengeId: state.Login.challengeId
-    }))
+	const initialValues = React.useMemo(() => {
+		return {
+			email: "",
+			code: ""
+		}
+	}, [])
 
+	const validationSchema = React.useMemo(() => {
+		const validationObj = {
+			email: Yup.string().required("Email required").email("Email is invalid")
+		}
+		tabIndex === OTP_CODE_INPUT_FORM_TAB_INDEX &&
+			Object.assign(validationObj, {
+				code: Yup.string().required("Code required")
+			})
+		return Yup.object().shape(validationObj)
+	}, [tabIndex])
 
-    useEffect(() => {
-        setTimeout(() => {
-            dispatch(resetLoginFlag());
-        }, 3000);
-    }, [dispatch, error]);
+	const handleSubmit = () => {
+		if (tabIndex === EMAIL_INPUT_FORM_TAB_INDEX) {
+			setTabIndex(OTP_CODE_INPUT_FORM_TAB_INDEX)
+		} else {
+			// api verify otp...
+		}
+	}
 
-    document.title = T('Application Name');
-    return (
-        <React.Fragment>
-            <ParticlesAuth>
-                <div className="auth-page-content">
-                    <Container>
-                        <Row>
-                            <Col lg={12}>
-                                <div className="text-center mt-sm-5 mb-4 text-white-50">
-                                    <p className="mt-3 fs-15 fw-medium">{T('Application Name')}</p>
-                                </div>
-                            </Col>
-                        </Row>
+	useEffect(() => {
+		setTimeout(() => {
+			dispatch(resetLoginFlag())
+		}, 3000)
+	}, [dispatch, error])
 
-                        <Row className="justify-content-center">
-                            <Col md={8} lg={6} xl={5}>
-                                {challengeId ? (
-                                    <Card className="mt-4">
-                                        <CardBody className="p-4">
-                                            <div className="text-center mt-2">
-                                                <h5 className="text-primary">{T('Welcome Back!')}</h5>
-                                            </div>
-                                            {error && error ? (<Alert color="danger"> {error} </Alert>) : null}
-                                            <div className="p-2 mt-4">
-                                                <Form
-                                                    onSubmit={(e) => {
-                                                        e.preventDefault();
-                                                        validation.handleSubmit();
-                                                        return false;
-                                                    }}
-                                                    action="#">
+	document.title = T("Application Name")
 
-                                                    <div className="mb-3">
-                                                        <Label htmlFor="email" className="form-label">{T('Email')}</Label>
-                                                        <Input
-                                                            name="email"
-                                                            className="form-control"
-                                                            placeholder="Enter email"
-                                                            type="email"
-                                                            onChange={validation.handleChange}
-                                                            onBlur={validation.handleBlur}
-                                                            value={validation.values.email || ""}
-                                                            invalid={
-                                                                validation.touched.email && validation.errors.email ? true : false
-                                                            }
-                                                        />
-                                                        {validation.touched.email && validation.errors.email ? (
-                                                            <FormFeedback type="invalid">{validation.errors.email}</FormFeedback>
-                                                        ) : null}
-                                                    </div>
+	return (
+		<React.Fragment>
+			<Formik
+				ref={formikRef}
+				initialValues={initialValues}
+				validationSchema={validationSchema}
+				onSubmit={handleSubmit}
+			>
+				{({
+					values,
+					errors,
+					touched,
+					handleChange,
+					handleBlur,
+					handleSubmit
+				}) => (
+					<ParticlesAuth>
+						<div className="auth-page-content">
+							<Container>
+								<Row>
+									<Col lg={12}>
+										<div className="text-center mt-sm-5 mb-4 text-white-50">
+											<p className="mt-3 fs-15 fw-medium">
+												{T("Application Name")}
+											</p>
+										</div>
+									</Col>
+								</Row>
 
-                                                    <div className="mt-4">
-                                                        <Button color="success" className="btn btn-success w-100" type="submit">{T('Get OTP')}</Button>
-                                                    </div>
-                                                </Form>
-                                            </div>
-                                        </CardBody>
-                                    </Card>
-                                ) : (<Card className="mt-4">
-                                    <CardBody className="p-4">
-                                        <div className="text-center mt-2">
-                                            <h5 className="text-primary">{T('Welcome Back!')}</h5>
-                                        </div>
-                                        {error && error ? (<Alert color="danger"> {error} </Alert>) : null}
-                                        <div className="p-2 mt-4">
-                                            <Form
-                                                onSubmit={(e) => {
-                                                    e.preventDefault();
-                                                    validation.handleSubmit();
-                                                    return false;
-                                                }}
-                                                action="#">
+								<Row className="justify-content-center">
+									<Col md={8} lg={6} xl={5}>
+										<Card className="mt-4">
+											<CardBody className="p-4">
+												<div className="text-center mt-2">
+													<h5 className="text-primary">
+														{tabIndex === EMAIL_INPUT_FORM_TAB_INDEX
+															? "Login"
+															: "Verify OTP code"}
+													</h5>
+												</div>
+												{error && error ? (
+													<Alert color="danger"> {error} </Alert>
+												) : null}
+												<div className="p-2 mt-4">
+													<Form onSubmit={handleSubmit} action="#">
+														{tabIndex === EMAIL_INPUT_FORM_TAB_INDEX ? (
+															<div className="mb-3">
+																<Label htmlFor="email" className="form-label">
+																	{T("Email")}
+																</Label>
+																<Input
+																	name="email"
+																	className="form-control"
+																	placeholder="Enter email"
+																	type="email"
+																	onChange={handleChange}
+																	onBlur={handleBlur}
+																	value={values.email}
+																	invalid={
+																		touched.email && errors.email ? true : false
+																	}
+																/>
+																{!!touched.email && !!errors.email ? (
+																	<FormFeedback>{errors.email}</FormFeedback>
+																) : null}
+															</div>
+														) : (
+															<div className="mb-3">
+																<Label htmlFor="email" className="form-label">
+																	{T("OTP code")}
+																</Label>
+																<Input
+																	name="code"
+																	className="form-control"
+																	placeholder="OTP code"
+																	type="password"
+																	onChange={handleChange}
+																	onBlur={handleBlur}
+																	value={values.code}
+																	invalid={
+																		touched.code && errors.code ? true : false
+																	}
+																/>
+																{!!touched.code && !!errors.code ? (
+																	<FormFeedback>{errors.code}</FormFeedback>
+																) : null}
+															</div>
+														)}
 
-                                                <div className="mb-3">
-                                                    <Label htmlFor="email" className="form-label">{T('Email')}</Label>
-                                                    <Input
-                                                        name="email"
-                                                        className="form-control"
-                                                        placeholder="Enter email"
-                                                        type="email"
-                                                        onChange={validation.handleChange}
-                                                        onBlur={validation.handleBlur}
-                                                        value={validation.values.email || ""}
-                                                        invalid={
-                                                            validation.touched.email && validation.errors.email ? true : false
-                                                        }
-                                                    />
-                                                    {validation.touched.email && validation.errors.email ? (
-                                                        <FormFeedback type="invalid">{validation.errors.email}</FormFeedback>
-                                                    ) : null}
-                                                </div>
+														<div className="mt-4">
+															<Button
+																color="success"
+																className="btn btn-success w-100"
+																type="submit"
+															>
+																{tabIndex === EMAIL_INPUT_FORM_TAB_INDEX
+																	? "Get OTP"
+																	: "Sign in"}
+															</Button>
+														</div>
+													</Form>
+												</div>
+											</CardBody>
+										</Card>
+										{/* {challengeId ? (
+											<Card className="mt-4">
+												<CardBody className="p-4">
+													<div className="text-center mt-2">
+														<h5 className="text-primary">
+															{T("Welcome Back!")}
+														</h5>
+													</div>
+													{error && error ? (
+														<Alert color="danger"> {error} </Alert>
+													) : null}
+													<div className="p-2 mt-4">
+														<Form
+															onSubmit={(e) => {
+																e.preventDefault()
+																validation.handleSubmit()
+																return false
+															}}
+															action="#"
+														>
+															<div className="mb-3">
+																<Label htmlFor="email" className="form-label">
+																	{T("Email")}
+																</Label>
+																<Input
+																	name="email"
+																	className="form-control"
+																	placeholder="Enter email"
+																	type="email"
+																	onChange={validation.handleChange}
+																	onBlur={validation.handleBlur}
+																	value={validation.values.email || ""}
+																	invalid={
+																		validation.touched.email &&
+																		validation.errors.email
+																			? true
+																			: false
+																	}
+																/>
+																{validation.touched.email &&
+																validation.errors.email ? (
+																	<FormFeedback type="invalid">
+																		{validation.errors.email}
+																	</FormFeedback>
+																) : null}
+															</div>
 
-                                                <div className="mt-4">
-                                                    <Button color="success" className="btn btn-success w-100" type="submit">{T('Get OTP')}</Button>
-                                                </div>
-                                            </Form>
-                                        </div>
-                                    </CardBody>
-                                </Card>)}
+															<div className="mt-4">
+																<Button
+																	color="success"
+																	className="btn btn-success w-100"
+																	type="submit"
+																>
+																	{T("Get OTP")}
+																</Button>
+															</div>
+														</Form>
+													</div>
+												</CardBody>
+											</Card>
+										) : (
+											<Card className="mt-4">
+												<CardBody className="p-4">
+													<div className="text-center mt-2">
+														<h5 className="text-primary">
+															{T("Welcome Back!")}
+														</h5>
+													</div>
+													{error && error ? (
+														<Alert color="danger"> {error} </Alert>
+													) : null}
+													<div className="p-2 mt-4">
+														<Form
+															onSubmit={(e) => {
+																e.preventDefault()
+																validation.handleSubmit()
+																return false
+															}}
+															action="#"
+														>
+															<div className="mb-3">
+																<Label htmlFor="email" className="form-label">
+																	{T("Email")}
+																</Label>
+																<Input
+																	name="email"
+																	className="form-control"
+																	placeholder="Enter email"
+																	type="email"
+																	onChange={validation.handleChange}
+																	onBlur={validation.handleBlur}
+																	value={validation.values.email || ""}
+																	invalid={
+																		validation.touched.email &&
+																		validation.errors.email
+																			? true
+																			: false
+																	}
+																/>
+																{validation.touched.email &&
+																validation.errors.email ? (
+																	<FormFeedback type="invalid">
+																		{validation.errors.email}
+																	</FormFeedback>
+																) : null}
+															</div>
 
+															<div className="mt-4">
+																<Button
+																	color="success"
+																	className="btn btn-success w-100"
+																	type="submit"
+																>
+																	{T("Get OTP")}
+																</Button>
+															</div>
+														</Form>
+													</div>
+												</CardBody>
+											</Card>
+										)} */}
+									</Col>
+								</Row>
+							</Container>
+						</div>
+					</ParticlesAuth>
+				)}
+			</Formik>
+		</React.Fragment>
+	)
+}
 
-                            </Col>
-                        </Row>
-                    </Container>
-                </div>
-            </ParticlesAuth>
-        </React.Fragment>
-    );
-};
-
-export default withRouter(withTranslation()(Login));
+export default withRouter(withTranslation()(Login))
